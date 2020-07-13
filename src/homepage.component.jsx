89b35d876aef4fe6c69104f863721999fa03f8bf
nathanganesh <nathangemieee@gmail.com>
2020-07-13 20:51:28 +0200

    ecom push

---
+++ src/homepage.component.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './homepage.style.scss';
+
+const HomePage = () => {
+    return <div className = 'homepage'>
+        <div className='menu-item'>
+            <div className='content'>
+                <h1 className='title'>shopnow</h1>
+                <span className='subtitle'>sneakers</span>
+            </div>
+        </div>
+         </div>
+}
+
+export default HomePage;
